3cb6a252097ece60a1f42e122e0bb2eff8a7bf69
majaliju <majalijunyc@gmail.com>
2022-03-28 18:34:45 -0400

    commit tree 62551513c2756399240577fa45e76e82b1971fd6

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // one of the optimal versions from the video following the initial git lesson
+
+
+
+  for (let i = 0; i < array.length; i++){
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === complement) return true;
+    }
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here  
 */
 
-/* 
-  Add your pseudocode here
+
+
+/* my initial approach -- the non-optimal method, just a first draft
+  a pyramid style problem 
+for the A number in the array 
+  for the B number in the array 
+    add these two numbers and check if equal target
+    if yes --> return true
+    if no --> check A + C, A + D, etc until the array runs out 
+    then start again with the B number, and add the +C, + D numbers etc
 */
+
 
 /*
   Add written explanation of your solution here
